1ba0539e7635efe47ed904a2ce1c24db6885774d
karinazyat <kzyat@msn.com>
2022-03-01 16:24:53 -0700

    finally figured out date time range!!

--- app.R
+++ app.R
@@ -17,6 +17,7 @@ library(shinythemes)
 library(shinyjs)
 library(plotly)
 library(zoo)
+library(stringr)
 # library(scales)
 # library(zoo)
 
@@ -67,7 +68,18 @@ ui <- bootstrapPage(theme = shinytheme("sandstone"),
       numericInput(inputId = "dControl",
                    label = "Control Displacement Digit 0",
                    # value = 49.37926)
-                   value = 4234.015)
+                   value = 4234.015),
+      conditionalPanel(condition = "output.fileUploaded == true",
+                       daterangepicker("dRange", label = "Select Date Range",
+                                       options = daterangepickerOptions(
+                                         
+                                         showDropdowns = T,
+                                         showCustomRangeLabel = T,
+                                         timePicker = T,
+                                         timePickerIncrement = c(0,10,15,20,30,40,45, 50, 60),
+                                         timePicker24Hour = T
+                                       ))),
+      actionButton("action", "Reset!")
       
       ), # sidebar panel
       
@@ -97,9 +109,16 @@ ui <- bootstrapPage(theme = shinytheme("sandstone"),
 ) #end UI
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+
+# get Data input and merge files ------------------------------------------
+
   
   getData <- reactive({
+    validate(
+      need(input$file1 != "", "Please upload a file")
+    )
     inFile <- input$file1
     if (is.null(inFile)){
       return(NULL)
@@ -113,7 +132,7 @@ server <- function(input, output) {
       {
         
         data.dat <- read.table(input$file1[[i, 'datapath']], skip = 1,
-                               header = T, sep = ',', na.strings = 'NAN')
+                               header = T, sep = ',', na.strings = 'NAN', stringsAsFactors = F)
         #, row.names = 2)
         
         #data.dat$TIMESTAMP <- as.POSIXct(data.dat$TIMESTAMP)
@@ -130,10 +149,14 @@ server <- function(input, output) {
         # comment out for now while playing
       #  merged_data[[i]] <- data.dat[-c(1:2),]
         
-        #if(data.dat$TIMESTAMP[1] == 'TS') {
+        # { if(data.dat$TIMESTAMP[1] == 'TS') 
           
        
-        merged_data[[i]] <- data.dat[-c(1:2),]
+        merged_data[[i]] <- data.dat[-c(1:2),] 
+          
+        #   else merged_data[[i]] <- data.dat
+        # }
+        # data.dat[-c(1:2),]
         # }
         # else {
         #   merged_data[[i]] <- data.dat
@@ -167,7 +190,26 @@ server <- function(input, output) {
         #select(c(TIMESTAMP, raC.24, raT.24, raL.24, raU.24)) %>% 
     
     }
+    # if(str_detect(data.bound$TIMESTAMP[1] == "^[:digit:]{1}[:punct:]")) {
+    # data.bound$TIMESTAMP <- as.POSIXct(parse_date_time(data.bound$TIMESTAMP,"mdy HM"))
+    # }
+    # else{
+    observe(print(str(data.bound)))
     data.bound$TIMESTAMP <- as.POSIXct(data.bound$TIMESTAMP)
+    
+    # data.bound$TIMESTAMP <- as.POSIXct(parse_date_time(data.bound$TIMESTAMP,"mdy HM"))
+    
+    # data.bound$TIMESTAMP <- if_else(grepl("^[:digit:][:punct:]",data.bound$TIMESTAMP),
+    #                                 as.POSIXct(parse_date_time(data.bound$TIMESTAMP,"mdy HM")),
+    #                                 as.POSIXct(data.bound$TIMESTAMP))
+    
+    
+      # as.POSIXct(data.bound$TIMESTAMP)
+    # }
+
+# equations displacement --------------------------------------------------
+
+    
     data.bound[is.na(data.bound)] <- 0
     lower <- (input$dLower -2631) * 0.02828 + (22.76 - 22.8) * ##TT_Lower = 22.76
       (((input$dLower * 0.000384) + (-0.3482)) * 0.02828)
@@ -182,6 +224,10 @@ server <- function(input, output) {
     data.bound %>%
       #group_by(TIMESTAMP) %>% 
      # arrange(desc(TIMESTAMP)) %>%
+      # mutate(TIMESTAMP = if_else(grepl("^[:digit:][:punct:]", as.character(TIMESTAMP)),
+      #        #TIMESTAMP = if_else(grepl("^[:digit:][:punct:]", as.character(TIMESTAMP)
+      #               as.POSIXct(parse_date_time(TIMESTAMP,"mdy HM")),
+      #               as.POSIXct(TIMESTAMP))) %>% 
       arrange(TIMESTAMP) %>%
       mutate(across(where(is.character), as.numeric)) %>%
       
@@ -217,79 +263,122 @@ server <- function(input, output) {
     # 
     # #remove NaN values -- not sure any are actually here
     # data.bound[is.nan(data.bound)] <- 0
-    
+    # updateDateRangeInput(session, "range", label = "Select Date Range", 
+    #                      min = min(data.bound$TIMESTAMP),  
+    #                      max = max(data.bound$TIMESTAMP))
   })
 
   observe(print(str(getData())))
   
+  output$fileUploaded <- reactive({
+    return(!is.null(getData()))
+  })
+  
+  outputOptions(output, 'fileUploaded', suspendWhenHidden = F)
+
+# dates -------------------------------------------------------------------
+
+
+  observeEvent(input$action, {
+    getData <- getData()
+    dates <-  getData$TIMESTAMP
+    minval <- as.character(min(dates))
+    maxval <- as.character(max(dates))
+    
+    updateDaterangepicker(
+      
+      session, "dRange",
+      start = minval, end = maxval,
+      min = minval,  max = maxval
+      
+    )
+  })
+
   date.character <- reactive ({
     #use validate and need to suppress warning messages
     #when no input file
     validate(
       need(input$file1 != "", "Please upload a file")
     )
-    getData() %>% 
-      mutate_if(is.POSIXct, as.character)
-     })
-  
-  output$contents <- DT::renderDT(
+    # minRowVal <- which(grepl(input$range[1], getData()$TIMESTAMP))
+    # maxRowVal <- which(grepl(input$range[2], getData()$TIMESTAMP))
+  getData() %>%
+  #   filter(
+  # TIMESTAMP >= input$range[1] & TIMESTAMP <= input$range[2]
+  # )
+  mutate_if(is.POSIXct, as.character) %>% 
+    filter(TIMESTAMP >= format(input$dRange[1]) &
+             TIMESTAMP <= format(input$dRange[2]) )
+  })
+
+  output$contents <- DT::renderDT({
     
-      date.character(),
+      # DT:: datatable(date.character()[minRowVal(): maxRowVal(), ],
+      DT:: datatable(date.character(),
+      # DT:: datatable( getData(),
     filter = 'top'
     
   )
+  })
+  
+  # observe({
+  #   x <- inpu
+  #     filter(
+  #       TIMESTAMP >= input$range[1] & TIMESTAMP <= input$range[2]
+  #     )
+  # })
   
   
-    
-    output$plot <- renderPlotly({
+      output$plot <- renderPlotly({
       
-     #browser()
+      #browser()
       
-     getData <- getData()
+      getData <- getData()
       
-     # getData$TIMESTAMP <- as.POSIXct(getData$TIMESTAMP)
+      # getData$TIMESTAMP <- as.POSIXct(getData$TIMESTAMP)
       
-     # getData %>%
-     #   # mutate(across(where(is.character), as.numeric)) %>%
-     #    mutate(raT.24 = rollapply(T109_C, 96, mean, align = 'right', fill = 0)) %>%
-     #    mutate(disp.lower = (Digits_Lower -2631) * 0.02828 + (TT_Lower - 22.8) *
-     #             (((Digits_Lower * 0.000384) + (-0.3482)) * 0.02828)) %>%
-     #    mutate(norm.dispL = disp.lower- disp.lower[1]) %>%
-     #    mutate(raL.24 = rollapply(norm.dispL, 96, mean, align = 'right', fill = 0)) %>%
-     #    mutate(disp.upper = (Digits_Upper -2584) * 0.02828 + (TT_Upper - 22.8) *
-     #             (((Digits_Upper * 0.000384) + (-0.3482)) * 0.02828)) %>%
-     #    mutate(norm.dispU = disp.upper- disp.upper[1]) %>%
-     #    mutate(raU.24 = rollapply(norm.dispU, 96, mean, align = 'right', fill = 0)) %>%
-     #    mutate(disp.control = (Digits_CTRL -2520) * 0.02869 + (TT_CTRL - 22.8) *
-     #             (((Digits_CTRL * 0.000384) + (-0.3482)) * 0.02869)) %>%
-     #    mutate(norm.dispC = disp.control- disp.control[1]) %>%
-     #    mutate(raC.24 = rollapply(norm.dispC, 96, mean, align = 'right', fill = 0)) %>%
+      # getData %>%
+      #   # mutate(across(where(is.character), as.numeric)) %>%
+      #    mutate(raT.24 = rollapply(T109_C, 96, mean, align = 'right', fill = 0)) %>%
+      #    mutate(disp.lower = (Digits_Lower -2631) * 0.02828 + (TT_Lower - 22.8) *
+      #             (((Digits_Lower * 0.000384) + (-0.3482)) * 0.02828)) %>%
+      #    mutate(norm.dispL = disp.lower- disp.lower[1]) %>%
+      #    mutate(raL.24 = rollapply(norm.dispL, 96, mean, align = 'right', fill = 0)) %>%
+      #    mutate(disp.upper = (Digits_Upper -2584) * 0.02828 + (TT_Upper - 22.8) *
+      #             (((Digits_Upper * 0.000384) + (-0.3482)) * 0.02828)) %>%
+      #    mutate(norm.dispU = disp.upper- disp.upper[1]) %>%
+      #    mutate(raU.24 = rollapply(norm.dispU, 96, mean, align = 'right', fill = 0)) %>%
+      #    mutate(disp.control = (Digits_CTRL -2520) * 0.02869 + (TT_CTRL - 22.8) *
+      #             (((Digits_CTRL * 0.000384) + (-0.3482)) * 0.02869)) %>%
+      #    mutate(norm.dispC = disp.control- disp.control[1]) %>%
+      #    mutate(raC.24 = rollapply(norm.dispC, 96, mean, align = 'right', fill = 0)) %>%
       
-  plot_ly(getData) %>% 
-  add_trace(x = ~TIMESTAMP, y = ~raT.24, type = "scatter", 
-            mode = "markers", marker = list(color = "#00AFBB"), yaxis = "y2", name = "Temperature") %>%
-  add_trace(x = ~TIMESTAMP, y = ~raL.24,
-            type = "scatter", mode = "markers", marker = list(color = '#00FF00'),
-            name = "Lower Displacement", textposition = "top center") %>%
-  add_trace(x = ~TIMESTAMP, y = ~raU.24, type = "scatter", 
-            mode = "markers", marker = list(color = "#E7B800"), name = "Upper Displacement") %>%
-  add_trace(x = ~TIMESTAMP, y = ~raC.24, type = "scatter", 
-            mode = "markers", marker = list(color = '#FF0033'), name = "Control") %>%
-  add_trace(x = ~TIMESTAMP, y = ~raL.noT,
-            type = "scatter", mode = "markers", marker = list(color = '#00FF00'),
-            name = "Lower Displacement No Temp", textposition = "top center") %>%
-  add_trace(x = ~TIMESTAMP, y = ~raU.noT, type = "scatter", 
-            mode = "markers", marker = list(color = "#E7B800"), name = "Upper Displacement No Temp") %>%
-  add_trace(x = ~TIMESTAMP, y = ~raC.noT, type = "scatter", 
-            mode = "markers", marker = list(color = '#FF0033'), name = "Control No Temp") %>%
-  layout(
-        legend = list(orientation = 'v'),
-        yaxis2 = list(side = "right", title = "Temperature (deg C)"),
-        title = "Pine Creek Housing Crackmeter Displacement (24 hour rolling average data)",
-        xaxis = list(title="Time"),
-        yaxis = list(overlaying = "y2", title="Displacement (mm)")
+      plot_ly(getData) %>% 
+        add_trace(x = ~TIMESTAMP, y = ~raT.24, type = "scatter", 
+                  mode = "markers", marker = list(color = "#00AFBB"), yaxis = "y2", name = "Temperature") %>%
+        add_trace(x = ~TIMESTAMP, y = ~raL.24,
+                  type = "scatter", mode = "markers", marker = list(color = '#00FF00'),
+                  name = "Lower Displacement", textposition = "top center") %>%
+        add_trace(x = ~TIMESTAMP, y = ~raU.24, type = "scatter", 
+                  mode = "markers", marker = list(color = "#E7B800"), name = "Upper Displacement") %>%
+        add_trace(x = ~TIMESTAMP, y = ~raC.24, type = "scatter", 
+                  mode = "markers", marker = list(color = '#FF0033'), name = "Control") %>%
+        add_trace(x = ~TIMESTAMP, y = ~raL.noT,
+                  type = "scatter", mode = "markers", marker = list(color = '#00FF00'),
+                  name = "Lower Displacement No Temp", textposition = "top center") %>%
+        add_trace(x = ~TIMESTAMP, y = ~raU.noT, type = "scatter", 
+                  mode = "markers", marker = list(color = "#E7B800"), name = "Upper Displacement No Temp") %>%
+        add_trace(x = ~TIMESTAMP, y = ~raC.noT, type = "scatter", 
+                  mode = "markers", marker = list(color = '#FF0033'), name = "Control No Temp") %>%
+        layout(
+          legend = list(orientation = 'v'),
+          yaxis2 = list(side = "right", title = "Temperature (deg C)"),
+          title = "Pine Creek Housing Crackmeter Displacement (24 hour rolling average data)",
+          xaxis = list(title="Time"),
+          yaxis = list(overlaying = "y2", title="Displacement (mm)")
         )
-        })
+    })
+    
 
 }
 
